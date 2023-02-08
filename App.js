44beb3be4171eba32badb116a110f2f48172c200
Michał Karteczka <116381179+karteczqa-sql@users.noreply.github.com>
2023-02-08 19:52:09 +0100

    mk1

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import _ from "lodash";
+
+import emoji from "./emoji";
 
 export default function App() {
+  const aspectRatio = window.innerHeight / window.innerWidth;
+  const rows = Number(5 * aspectRatio);
+  const columns = 5;
+
+  const randomColor = (() => {
+    "use strict";
+
+    const randomInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    return () => {
+      var h = randomInt(0, 360);
+      var s = randomInt(42, 98);
+      var l = randomInt(40, 90);
+      return `hsl(${h},${s}%,${l}%)`;
+    };
+  })();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "white",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    button: {
+      alignItems: "center",
+      justifyContent: "center",
+      width: (window.innerWidth / columns) * 0.99,
+      height: window.innerHeight / rows,
+      zIndex: -1000,
+      borderRadius: 30,
+    },
+    row: {
+      flexDirection: "row",
+    },
+    emoji: {
+      fontSize: 25,
+    },
+  });
+
+  const Row = ({ children }) => <View style={styles.row}>{children}</View>;
+
+  const MyButton = ({ b_color }) => (
+    <TouchableOpacity>
+      <View
+        key={Math.random()}
+        style={{
+          ...styles.button,
+          backgroundColor: randomColor(),
+        }}
+      >
+        <Text style={styles.emoji}>
+          {String.fromCodePoint(0x1f600 + Math.floor(Math.random() * 200))}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {_.times(rows, (i) => (
+        <View style={styles.app}>
+          <Row>
+            {_.times(columns, (i) => (
+              <MyButton key={i} b_color="#aaa" />
+            ))}
+          </Row>
+        </View>
+      ))}
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
